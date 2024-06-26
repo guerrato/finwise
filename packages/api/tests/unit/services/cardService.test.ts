ca6bcb48ee29d0250a9a2d3fd00ff52e6b64d6ac
Pedro Guerrato <pguerrato@gmail.com>
2024-06-26 02:02:27 -0300

    Increase DB models entities

---
+++ packages/api/tests/unit/services/cardService.test.ts
@@ -0,0 +1,19 @@
+test('Credit card must have due date', () => {
+  return expect(false).toBe(true);
+})
+
+test('Card must have issuer bank', () => {
+  return expect(false).toBe(true);
+})
+
+test('Card must have network', () => {
+  return expect(false).toBe(true);
+})
+
+test('Card must have issuer bank when network is Credit', () => {
+  return expect(false).toBe(true);
+})
+
+test('Card must have issuer bank when network is Debit', () => {
+  return expect(false).toBe(true);
+})
---
+++ packages/api/tests/unit/services/outflowService.test.ts
@@ -0,0 +1,82 @@
+test('Should throw error if outflow does not have due date', () => {
+  return expect(false).toBe(true)
+})
+
+describe('Outflow Payment Details', () => {
+  test('Should throw error if outflow payment type is PIX and has no bank', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is PIX and has no bank', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is TRANSFER and has no bank', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is BOLETO and has no bank', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is TED and has no bank', () => {
+    return expect(false).toBe(true)
+  })
+  
+  test('Should throw error if outflow payment type is DOC and has no bank', () => {
+    return expect(false).toBe(true)
+  })
+  
+  test('Should throw error if outflow payment type is DEPOSIT and has no bank', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is DEBIT_CARD and has no card', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is CREDIT_CARD and has no card', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is MONEY and bank is set', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is MONEY and card is set', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is PIX and card is set', () => {
+    return expect(false).toBe(true)
+  })
+  
+  test('Should throw error if outflow payment type is DEBIT_CARD and bank is set', () => {
+    return expect(false).toBe(true)
+  })
+  
+  test('Should throw error if outflow payment type is CREDIT_CARD and bank is set', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is TRANSFER and card is set', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is BOLETO and card is set', () => {
+    return expect(false).toBe(true)
+  })
+
+  test('Should throw error if outflow payment type is TED and card is set', () => {
+    return expect(false).toBe(true)
+  })
+  
+  test('Should throw error if outflow payment type is DOC and card is set', () => {
+    return expect(false).toBe(true)
+  })
+  
+  test('Should throw error if outflow payment type is DEPOSIT and card is set', () => {
+    return expect(false).toBe(true)
+  })
+
+})
